b6244147edbcf5fcbf3d5152473d36b09c99fb40
ninetynin <wekandmusic@gmail.com>
2022-10-31 22:32:26 +0530

    Add project files.

---
+++ Service1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Process.Start("w32tm", "/config /manualpeerlist:time.windows.com /syncfromflags:manual /reliable:yes /update");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
